e848239872af6e3461987c95afb1685bec438a00
ERKarl <dasigankarl919@gmail.com>
2025-08-27 23:52:43 -0700

    Add files via upload

---
+++ ConsoleCalc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int num1, num2, sum;
+
+    cout << "Hello," << name <<"! Let's add twoooo numberssszzaa." <<endl;
+    cout << "Enter the 1st number:";
+    cin >> num1;
+
+    cout << "Enter the second number:";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << "." << endl;
+    return 0;
+}
